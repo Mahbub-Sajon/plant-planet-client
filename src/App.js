799b06ceff56c7ad9537b31e0f0ce3e1be327f3f
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-04 01:21:32 +0600

    active link nav added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './component/Footer/Footer';
+import Home from './component/Home/Home';
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1>this is main file</h1>
+      <Navbar></Navbar>
+    <Routes>
+    <Route path='/home' element={<Home></Home>}></Route>
+    </Routes>
+    <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/HomeItems/HomeItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeItems = () => {
+    return (
+        <div>
+            <h1>this is home items</h1>
+        </div>
+    );
+};
+
+export default HomeItems;
