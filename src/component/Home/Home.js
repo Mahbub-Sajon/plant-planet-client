c237525f069c826da8b894c22f62e6620cd53177
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-16 13:57:55 +0600

    home products added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import useProducts from '../../hooks/useProducts'
+import Products from '../Products.js/Products';
 
 const Home = () => {
+    const [products, setProducts] = useProducts()
     return (
         <div>
             <h1>this is home</h1>
+            <div className='max-w-screen-xl m-auto mb-20'>
             <Banner></Banner>
+            </div>
+            <h1 className='text-lime-800 text-3xl font-bold pb-4'>Our Products</h1>
+            <div className='grid md:grid-cols-3 mb-10 gap-6'>
+                {
+                    products.slice(0, 6).map(product => <Products
+                        key={product._id}
+                        product = {product}
+                    ></Products>)
+                }
+
+
+
+            </div>
+           
+
         </div>
     );
 };
---
+++ src/component/Products.js/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Products = (props) => {
+    const { name, img, price, description, supplier, quantity } = props.product;
+    return (
+        <div className='border-2 border-lime-800 rounded-lg p-2'>
+            <img className='w-full h-80 rounded-lg' src={img} alt="" />
+            <h1> <span className='font-bold text-lime-800'>Name:</span>  {name}</h1>
+            <h4> <span className='font-bold text-lime-800'>Service Cost:</span> BDT {price}</h4>
+            <p><span className='font-bold text-lime-800'>Description:</span> {description}</p>
+            <p><span className='font-bold text-lime-800'>Supplier:</span> {supplier}</p>
+            <p><span className='font-bold text-lime-800'>Quantity:</span> {quantity}</p>
+            <div className='mx-auto w-32 h-12 bg-green-400 rounded-md pt-3 my-4 font-bold hover:bg-green-600 '>
+                <button>update</button>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
