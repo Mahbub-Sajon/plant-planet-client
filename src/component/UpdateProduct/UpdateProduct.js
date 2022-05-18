178bfb6d3bd6d31079cce12542a6ee0b51da75fe
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-18 13:57:12 +0600

    restock added

--- src/component/UpdateProduct/UpdateProduct.js
+++ src/component/UpdateProduct/UpdateProduct.js
@@ -3,16 +3,44 @@ import { useParams } from 'react-router-dom';
 
 const UpdateProduct = () => {
     const {id} = useParams();
-    const [product,setProduct] = useState({});
+    const [products,setProducts] = useState({});
     useEffect(() =>{
         const url = `http://localhost:5000/update-product/${id}`;
         fetch(url)
         .then(res => res.json())
-        .then(data => setProduct(data))
-    }, []) 
+        .then(data => setProducts(data))
+    }, []);
+
+    const handleAddProduct = event =>{
+        event.preventDefault();
+        const quantity = event.target.number.value;
+        const newQuantity = products.quantity + quantity;
+        const UpdateProduct = {newQuantity};
+        fetch('http://localhost:5000/products', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'aplication/json'
+            },
+            body: JSON.stringify(UpdateProduct)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success', data);
+            alert('users added successfully!!!');
+            event.target.reset();
+        })
+    }
     return (
         <div>
-            <h1>update product: {product.name}</h1>
+            <h1>Update Product:</h1>
+            <p> Name: {products.name}</p>
+            <p>Quantity:  {products.quantity}</p>
+
+            <form onSubmit={handleAddProduct}>
+                <input type="number" name="number" placeholder="Enter Quantity" />
+                <br />
+                <input type="submit" value="Restock" />
+            </form>
         </div>
     );
 };
